0208481a4728d2f4edf00af40a3eb2bdc0b86119
Dr. Schandorf Osam-Frimpong <osamfrimpong@yahoo.com>
2023-06-16 18:51:32 -0700

    Added 5-print_numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry Point
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	int start = 0;
+
+	while (start < 10)
+	{
+		printf("%d", start);
+		start++;
+	}
+
+	printf("\n");
+
+	return (0);
+}
