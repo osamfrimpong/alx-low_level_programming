6013854752b95a72dddfd066308c3de65843ee7b
Dr. Schandorf Osam-Frimpong <osamfrimpong@yahoo.com>
2023-06-20 22:37:17 -0700

    Added 11-print_to_98.c

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ *print_to_98 - print numbers to 98
+ *@n: character to be verified
+ *Return: returns void
+*/
+void print_to_98(int n)
+{
+	while (n <= 98)
+	{
+		_putchar('0' + n);
+		if (n < 98)
+			_putchar(',');
+			_putchar(' ');
+		n++;
+	}
+	_putchar(10);
+}
