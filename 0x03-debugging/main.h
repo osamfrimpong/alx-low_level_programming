c72bbfa587a2f7f55ea46757365d1f6448961694
Dr. Schandorf Osam-Frimpong <osamfrimpong@yahoo.com>
2023-06-21 20:29:25 -0700

    Added 0-main.c

---
+++ 0x03-debugging/main.h
@@ -0,0 +1,12 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+/**
+ * positive_or_negative - checks if a number is positive or negative
+ * @i: number to be checked
+ */
+
+void positive_or_negative(int i);
+
+#endif /* MAIN_H */
