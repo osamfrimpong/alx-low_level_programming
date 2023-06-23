ab75c6f84613904037417472c5bee06b00dfaf7c
Dr. Schandorf Osam-Frimpong <osamfrimpong@yahoo.com>
2023-06-22 18:34:13 -0700

    Fixed 0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,15 +1,12 @@
 #include "main.h"
 
-/*
+/**
  * _isupper - checks if supplied character is in upper case
  * Returns: 1 if true else 0
  */
 
 int _isupper(int c)
 {
-	int isUpper;
-	if(c >= 65 && c <= 90)
-		isUpper = 1;
-	return isUpper;
+	return (c >= 65 && c <= 90);
 }
 
