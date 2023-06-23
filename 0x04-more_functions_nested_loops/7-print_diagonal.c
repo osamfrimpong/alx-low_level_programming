276fc4fbfd5e27c3587ce3c8d8bd0150c7ae164d
Dr. Schandorf Osam-Frimpong <osamfrimpong@yahoo.com>
2023-06-22 22:21:33 -0700

    Edited 7-print_diagonal.c

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -10,7 +10,7 @@ void print_diagonal(int n)
 {
 	if (n <= 0)
 	{
-		putchar('\n');
+		_putchar('\n');
 	} else
 	{
 		int i, j;
@@ -20,11 +20,11 @@ void print_diagonal(int n)
 			for (j = 0; j < n; j++)
 			{
 				if (j == i)
-					putchar('\\');
+					_putchar('\\');
 				else if (j < i)
-					putchar(' ');
+					_putchar(' ');
 			}
-			putchar('\n');
+			_putchar('\n');
 		}
 	}
 }
