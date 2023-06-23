51ba361ae001ccb34243ffde6fa946828a56042b
Dr. Schandorf Osam-Frimpong <osamfrimpong@yahoo.com>
2023-06-22 18:40:30 -0700

    Added 1-isdigit.c

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _isdigit - checks if supplied character is a digit
+ * @c: character to check
+ * Return: 1 if true else 0
+ */
+
+int _isdigit(int c)
+{
+	return (c >= 48 && c <= 57);
+}
+
