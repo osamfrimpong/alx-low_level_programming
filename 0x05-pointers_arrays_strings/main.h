832fdd78c66be9390ee4f0f62bba8c0d24c0f160
Dr. Schandorf Osam-Frimpong <osamfrimpong@yahoo.com>
2023-06-27 05:35:40 +0000

    Added 9-strcpy.c

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -9,6 +9,8 @@ void print_rev(char *s);
 void puts2(char *str);
 void puts_half(char *str);
 void print_array(int *a, int n);
+char *_strcpy(char *dest, char *src);
+int _atoi(char *s);
 
 int _strlen(char *s)
 {
