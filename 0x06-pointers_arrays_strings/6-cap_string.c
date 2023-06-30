fcf85f7b2bf767c47fc74f8752b04679c3c6998b
Dr. Schandorf Osam-Frimpong <osamfrimpong@yahoo.com>
2023-06-30 01:18:34 +0000

    Fixed error in 6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -9,15 +9,16 @@
 char *cap_string(char *str)
 {
 	int capitalize_next = 1;
+	int i;
 
-	for (int i = 0; str[i] != '\0'; i++)
+	for (i = 0; str[i] != '\0'; i++)
 	{
 		if (capitalize_next && (str[i] >= 'a' && str[i] <= 'z'))
 		{
 			str[i] -= 32;
 		}
 
-		if (str[i] == ' ' || str[i] == '    '
+		if (str[i] == ' ' || str[i] == 9
 				|| str[i] == '\n' || str[i] == ',' || str[i] == ';'
 				|| str[i] == '?' || str[i] == '.' || str[i] == '!'
 				|| str[i] == '\"' || str[i] == '(' || str[i] == ')'
