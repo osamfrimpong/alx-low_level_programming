320c17e965f667155c64a2654d6497cb9bc60dfc
Dr. Schandorf Osam-Frimpong <osamfrimpong@yahoo.com>
2023-06-30 02:04:23 +0000

    Added 101-print_number.c

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -11,7 +11,7 @@ char *cap_string(char *str);
 char *leet(char *str);
 char *rot13(char *str);
 void _putchar(int c);
-voit print_number(int n);
+void print_number(int n);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
 void print_buffer(char *b, int size);
 
