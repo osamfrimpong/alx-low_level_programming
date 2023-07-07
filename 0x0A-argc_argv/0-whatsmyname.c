033385307af8a8a05db1d6887435355e8fb39df7
Dr. Schandorf Osam-Frimpong <osamfrimpong@yahoo.com>
2023-07-07 21:36:42 +0000

    Added 0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - entry point for programme
+ * @argc: argument count
+ * @argv: pointer to array of strings passed as arguments
+ * Return: 0 for success
+ */
+
+int main(int argc __attribute__((unused)), char *argv[])
+{
+	puts(argv[0]);
+	return (0);
+}
